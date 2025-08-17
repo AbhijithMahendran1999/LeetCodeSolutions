19a95a8610e783f926eccedc6c6b4de0907bea78
Abhijith Mahendran <abhithannickal@gmail.com>
2025-08-17 20:18:03 +1000

    Maximum depth of binary tree - BFS

--- src/MaximumDepthOfBinarytree/Solution.java
+++ src/MaximumDepthOfBinarytree/Solution.java
@@ -1,10 +1,24 @@
 package MaximumDepthOfBinarytree;
+import java.util.*;
 
 public class Solution {
     public int maxDepth(TreeNode root) {
         if(root ==null) return 0;
-//        if(root.left == null && root.right == null) return 1;
 
-        return 1 + (Math.max(maxDepth(root.left),maxDepth(root.right)));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth =0;
+        while(!q.isEmpty()){
+            depth++;
+            int levelsize = q.size();
+            for(int i =0; i<levelsize; i++){
+                TreeNode node = q.poll();
+                if(node.left != null)
+                    q.add(node.left);
+                if(node.right !=null)
+                    q.add(node.right);
+            }
+        }
+        return depth;
     }
 }
