31f60e92cb5341bc33df195f7403d13c7a19a391
Abhijith Mahendran <abhithannickal@gmail.com>
2025-08-17 16:54:05 +1000

    Merge two sorted linked list - dummy pointer approach

--- src/MergeTwoSortedLinkedList/Solution.java
+++ src/MergeTwoSortedLinkedList/Solution.java
@@ -2,15 +2,19 @@ package MergeTwoSortedLinkedList;
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-
-        if(list1.val <= list2.val ){
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        }else{
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+        ListNode dummy = new ListNode(), cur;
+        cur = dummy;
+        while(list1!= null && list2!=null){
+            if(list1.val <= list2.val ){
+                cur.next = list1;
+                list1 = list1.next;
+            }else{
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
         }
+        cur.next = (list1 != null) ? list1 : list2;
+        return dummy.next;
     }
 }
