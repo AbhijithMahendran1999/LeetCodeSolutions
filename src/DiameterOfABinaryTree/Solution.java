15c19bd31d9cbef911d1e911257fe2cba478029d
Abhijith Mahendran <abhithannickal@gmail.com>
2025-08-18 13:30:37 +1000

    Diameter of a binary tree - DFS

---
+++ src/DiameterOfABinaryTree/Solution.java
@@ -0,0 +1,20 @@
+package DiameterOfABinaryTree;
+
+public class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    public int dfs(TreeNode root){
+        if(root == null)
+            return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        max = Math.max(max, left+right);
+        
+        return 1 + Math.max(left,right);
+    }
+}
