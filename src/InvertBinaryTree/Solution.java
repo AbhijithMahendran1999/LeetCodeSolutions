a94bc2c4a830924b28aaef4519f5202397ea29ff
Abhijith Mahendran <abhithannickal@gmail.com>
2025-08-18 16:11:03 +1000

    Invert binary tree - DFS update

--- src/InvertBinaryTree/Solution.java
+++ src/InvertBinaryTree/Solution.java
@@ -1,5 +1,13 @@
 package InvertBinaryTree;
 
 public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        
+        TreeNode tempLeft = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tempLeft);
 
+        return root;
+    }
 }
